47065fc93fab4d924baca789035c5e9e1f44e842
emmanuel405 <emmanuel20.12@gmail.com>
2020-01-15 17:23:48 +0200

    printDown

--- trie.c
+++ trie.c
@@ -14,7 +14,7 @@ the method recived a word and node* to the head of the trie and insert the word 
 */
 void addWord(char* word, node* trie){
     int i;
-    node* n=trie;
+    node* n = trie;
     for(i=0; i<strlen(word); i++){
         n= addLetter(word[i], n);
     }
@@ -44,7 +44,6 @@ node* addLetter(char letter, node* n){
 
 void inorder(node *trie, char *word2print);
 void disorder(node *trie, char *word2print);
-
 char* subString(char *w, int i);
 
 /*
@@ -93,6 +92,9 @@ void printDown(node* trie){
 //*** private methodes ***//
 ////////////////////////////
 
+/*
+This method decrease 'i' letter from string 'w' by the end.
+*/
 char* subString(char *w, int i){
     int length = strlen(w);
     if(i <= length){
@@ -104,13 +106,12 @@ char* subString(char *w, int i){
 }
 /*
  * check what the word that we need to print
- * 
  */
 void inorder(node *trie, char *word2print){
     int numOfChild = strlen(trie->children);
     if(numOfChild == 0){ // he is a leaf
         if(trie->count != 0) {
-            printf("%s %d\n", word2print, trie->count);
+            printf("%s %d\n", word2print, trie->count); // print the word & num how many time it's writted
         }
         if(strlen(word2print) > 1)
             subString(word2print, 1);
@@ -118,35 +119,51 @@ void inorder(node *trie, char *word2print){
     }
     else{
         int i = 0;
-        while(i<numOfChild){
+        while(i<numOfChild){ // while he has a children, we need to go over them
             node *n = trie->children[i];
-            strcat(word2print, n->letter);
+            strcat(word2print, n->letter); // attach the letter to word2print
             if(n->count != 0){
                 printf("%s %d\n", word2print, n->count); 
             }
-            if(strlen(n->children) > 0){inorder(n, word2print);}
+            if(strlen(n->children) > 0){inorder(n, word2print);} // rekursia
             subString(word2print, 1);
             i++;
         }
-        if(i == numOfChild){
+        if(i == numOfChild){ // if we pass over all child, we can to return this father
             return;
         }
     }
 
 }
 
+/*
+ * check what the word that we need to print
+ */
 void disorder(node *trie, char *word2print){
     int numOfChild = strlen(trie->children);
     if(numOfChild == 0){ // he is a leaf
         if(trie->count != 0) {
-            printf("%s", word2print);
+            printf("%s %d\n", word2print, trie->count); // print the word & num how many time it's writted
         }
         if(strlen(word2print) > 1)
             subString(word2print, 1);
         return;
     }
     else{
-
+        int i = (numOfChild-1);
+        while(i >= 0){ // while he has a children, we need to go over them
+            node *n = trie->children[i];
+            strcat(word2print, n->letter); // attach the letter to word2print
+            if(n->count != 0){
+                printf("%s %d\n", word2print, n->count); 
+            }
+            if(strlen(n->children) > 0){disorder(n, word2print);} // rekursia
+            subString(word2print, 1);
+            i--;
+        }
+        if(i == -1){ // if we pass over all child, we can to return this father
+            return;
+        }
     }
 }
 
